e92104186e844807278ba0e752812e0ac4856d80
Matt Rice <mattrice12@outlook.com>
2016-07-18 17:08:57 -0500

    Have index and show pages for both list and tasks

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -4,4 +4,15 @@ class ListsController < ApplicationController
       lists: List.all,
     }
   end
+
+  def show
+    if List.exists?(params[:id])
+      render template: 'lists/show.html.erb', locals: {
+        tasks: Task.where(id: params[:id]),
+        list: List.find(params[:id]),
+      }
+    else
+      render html: "Not Found", status: 404
+    end
+  end
 end
--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,2 +1,15 @@
 class TasksController < ApplicationController
+  def index
+    render template: 'tasks/index.html.erb', locals: {
+      lists: List.all,
+      tasks: Task.where(id: params[:list_id]),
+      list: List.where(id: params[:list_id]),
+    }
+  end
+
+  def show
+    render template: 'tasks/show.html.erb', locals: {
+      tasks: Task.find(params[:list_id])
+    }
+  end
 end
